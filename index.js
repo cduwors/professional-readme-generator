e4e010241698ffa41740969f996b11b207934bed
cduwors <christinaduwors@gmail.com>
2022-07-30 13:16:37 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,138 @@
+// TODO: Include packages needed for this application
+const { writeFile } = require("fs");
+const { prompt } = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+	return prompt([
+		{
+			type: "input",
+			name: "title",
+			message: "What is your Project Title? (Required)",
+			validate: (titleInput) => {
+				if (titleInput) {
+					return true;
+				} else {
+					console.log("Please enter your Project's Title!");
+					return false;
+				}
+			},
+		},
+		// {
+		// 	type: "input",
+		// 	name: "description",
+		// 	message: "Write a description of your Project? (Required)",
+		// 	validate: (titleInput) => {
+		// 		if (titleInput) {
+		// 			return true;
+		// 		} else {
+		// 			console.log("Please type a description of your Project!");
+		// 			return false;
+		// 		}
+		// 	},
+		// },
+		// {
+		// 	type: "input",
+		// 	name: "installation",
+		// 	message:
+		// 		"What are the Installation Instructions for your Project? (Required)",
+		// 	validate: (titleInput) => {
+		// 		if (titleInput) {
+		// 			return true;
+		// 		} else {
+		// 			console.log(
+		// 				"Please type a Installation Directions for your Project!"
+		// 			);
+		// 			return false;
+		// 		}
+		// 	},
+		// },
+		// {
+		// 	type: "input",
+		// 	name: "usage",
+		// 	message: "What is the usage information for your Project? (Required)",
+		// 	validate: (titleInput) => {
+		// 		if (titleInput) {
+		// 			return true;
+		// 		} else {
+		// 			console.log("Please explain the usage information for your Project!");
+		// 			return false;
+		// 		}
+		// 	},
+		// },
+		// {
+		// 	type: "input",
+		// 	name: "contributing",
+		// 	message:
+		// 		"What are the contribution guidelines for your Project? (Required)",
+		// 	validate: (titleInput) => {
+		// 		if (titleInput) {
+		// 			return true;
+		// 		} else {
+		// 			console.log(
+		// 				"Please explain the contribution guidelines for your Project!"
+		// 			);
+		// 			return false;
+		// 		}
+		// 	},
+		// },
+		// {
+		// 	type: "input",
+		// 	name: "tests",
+		// 	message: "What are the test instructions for your Project? (Required)",
+		// 	validate: (titleInput) => {
+		// 		if (titleInput) {
+		// 			return true;
+		// 		} else {
+		// 			console.log("Please enter the test instructions for your Project!");
+		// 			return false;
+		// 		}
+		// 	},
+		// },
+		// {
+		// 	type: "list",
+		// 	name: "license",
+		// 	message: "Which license applies to your application? (Select one)",
+		// 	choices: ["MIT", "ISC", "GNU GPLv3", "Other community license"],
+		// },
+
+		// {
+		// 	type: "input",
+		// 	name: "gitHub",
+		// 	message: "What is your GitHub username? (Required)",
+		// 	validate: (titleInput) => {
+		// 		if (titleInput) {
+		// 			return true;
+		// 		} else {
+		// 			console.log("Please enter your GitHub username!");
+		// 			return false;
+		// 		}
+		// 	},
+		// },
+		// {
+		// 	type: "input",
+		// 	name: "email",
+		// 	message: "What is your email? (Required)",
+		// 	validate: (titleInput) => {
+		// 		if (titleInput) {
+		// 			return true;
+		// 		} else {
+		// 			console.log("Please enter your email!");
+		// 			return false;
+		// 		}
+		// 	},
+		// },
+	]);
+};
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+async function init() {
+	await questions();
+	console.log("init function");
+}
+
+// Function call to initialize app
+init();
